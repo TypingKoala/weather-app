222841c8c88cae9409b3a59b26b26b4c11ce81f8
Johnny Bui <typngeek@aim.com>
2020-12-28 16:27:30 -0500

    check for unique email on signup

--- controllers/register.js
+++ controllers/register.js
@@ -15,9 +15,19 @@ app.get('/register', (req, res) => {
   res.render('register', { sitekey: process.env.RECAPTCHA_SITE_KEY });
 });
 
+// Validate email is unique
+function validateEmailIsUnique(req, res, next) {
+  User.exists({ email: req.body.email }, (err, exists) => {
+    if (err) return next(err);
+    req.emailInUse = exists;
+    return next();
+  });
+}
+
 app.post(
   '/register',
   body('email').isEmail(),
+  validateEmailIsUnique,
   body('password').not().isEmpty(),
   validateRecaptcha,
   (req, res) => {
@@ -37,6 +47,11 @@ app.post(
     }
     if (req.body.password !== req.body.confirmpassword) {
       req.flash('error', 'Passwords do not match');
+      return res.redirect('/register');
+    }
+    // check if email is unique
+    if (req.emailInUse) {
+      req.flash('error', 'Email is already registered');
       return res.redirect('/register');
     }
 
